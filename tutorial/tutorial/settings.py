27495fd7402526a697e78184e93509f7d6a6f47c
jiangyuwei666 <739843128@qq.com>
2018-10-29 22:16:40 +0800

    1

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'tutorial.pipelines.TutorialPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'tutorial.pipelines.TextPipeline': 300,
+    'tutorial.pipelines.MongoPipeline': 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +89,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+MONGO_URI = 'localhost:27017'
+MONGO_DB = 'tutorial'
--- tutorial/tutorial/spiders/quotes.py
+++ tutorial/tutorial/spiders/quotes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from tutorial.items import QuotesItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -8,4 +9,23 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        quotes = response.xpath('//div[@class="quote"]')
+        for quote in quotes:
+            item = QuotesItem()
+            test1 = quote.xpath('.//span[@class="text"]/text()')
+            test2 = quote.xpath('.//span[@class="text"]/text()').extract()
+            test3 = quote.xpath('.//span[@class="text"]/text()').extract_first()
+            # item['text'] = quote.xpath('.//span[@class="text"]/text()').extract_first()
+            # item['author'] = quote.xpath('.//small[@class="author"]/text()').extract_first()
+            # item['tags'] = quote.xpath('.//a[@class="tag"]/text()').extract()
+            # print(item)
+            # yield item
+            print("*******")
+            print(test1)
+            print(test2)
+            print(test3)
+
+        # next_url = response.xpath('//nav//li[@class="next"]/a/@href').extract_first()
+        # url = response.urljoin(next_url)
+        # yield scrapy.Request(url=url, callback=self.parse)
+
