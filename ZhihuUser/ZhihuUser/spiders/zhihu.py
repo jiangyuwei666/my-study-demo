4eb507b8e9cdebf2dc2f921602f37e94c245c46c
jiangyuwei666 <739843128@qq.com>
2018-11-05 15:43:02 +0800

    数据库(MongoDB)done,等着将可视化加入

--- ZhihuUser/ZhihuUser/spiders/zhihu.py
+++ ZhihuUser/ZhihuUser/spiders/zhihu.py
@@ -17,7 +17,7 @@ class ZhihuSpider(scrapy.Spider):
     follows_url = 'https://www.zhihu.com/api/v4/members/{user_token}/followees?include={include}&limit={limit}&offset={offset}'
     follows_query = 'data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
     # 关注者的页面请求接口url和参数
-    followers_url = 'https://www.zhihu.com/api/v4/members/{user_token}/followers?include={followers_url}&offset={offset}&limit={limit}'
+    followers_url = 'https://www.zhihu.com/api/v4/members/{user_token}/followers?include={include}&offset={offset}&limit={limit}'
     followers_query = 'data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
     # 起始user
     start_user = 'excited-vczh'
@@ -26,13 +26,14 @@ class ZhihuSpider(scrapy.Spider):
 
     # 第一次的请求
     def start_requests(self):
-        # # 生成返回 个人信息 界面解析请求
-        # yield scrapy.Request(url=self.user_info_url.format(user_token=self.start_user, include=self.user_info_query), callback=self.parse_user)
-        # # 生成返回 我的关注 界面解析请求
-        # yield scrapy.Request(url=self.follows_url.format(user_token=self.start_user, include=self.follows_query, offset=20, limit=20), callback=self.parse_follow)
-        # # 生成返回 关注我的人 界面的解析请求
-        # yield scrapy.Request(url=self.followers_url.format(user_token=self.start_user, include=self.followers_query, offset=20, limit=20), callback=self.parse_follower)
-        yield scrapy.Request(url=self.test_url, callback=self.parse_test)
+        # 生成返回 个人信息 界面解析请求
+        yield scrapy.Request(url=self.user_info_url.format(user_token=self.start_user, include=self.user_info_query), callback=self.parse_user)
+        # 生成返回 我的关注 界面解析请求
+        yield scrapy.Request(url=self.follows_url.format(user_token=self.start_user, include=self.follows_query, offset=0, limit=20), callback=self.parse_follow)
+        # 生成返回 关注我的人 界面的解析请求
+        yield scrapy.Request(url=self.followers_url.format(user_token=self.start_user, include=self.followers_query, offset=0, limit=20), callback=self.parse_follower)
+        # 测试用
+        # yield scrapy.Request(url=self.test_url, callback=self.parse_test)
 
     # 个人信息的解析方法
     def parse_user(self, response):
@@ -45,7 +46,9 @@ class ZhihuSpider(scrapy.Spider):
                 item[field] = user_result.get(field)
         yield item
         # 返回所有关注的人的关注列表的请求到队列中
-        # yield scrapy.Request(url=self.follows_url.format(user_token=user_result.get('url_token'), include=self.follows_query, offset=0, limit=20), callback=self.parse_follow)
+        yield scrapy.Request(url=self.follows_url.format(user_token=user_result.get('url_token'), include=self.follows_query, offset=0, limit=20), callback=self.parse_follow)
+        # 返回所有人的粉丝列表的请求到队列
+        yield scrapy.Request(url=self.followers_url.format(user_token=user_result.get('url_token'), include=self.followers_query, offset=0, limit=20), callback=self.parse_follower)
 
     # 关注的人界面的解析方法
     def parse_follow(self, response):
@@ -58,22 +61,29 @@ class ZhihuSpider(scrapy.Spider):
         # 查看是否有下一页的的关注者，有就将下一页的请求返回队列
         if 'paging' in follow_result.keys():
             if follow_result.get('paging').get('is_end') is False:
-                yield scrapy.Request(url=follow_result.get('paging').get('next'), callback=self.parse_follow)
+                yield scrapy.Request(url=follow_result.get('paging').get('next').replace('https://www.zhihu.com/', 'https://www.zhihu.com/api/v4/'), callback=self.parse_follow)
 
     # 关注者界面（跟关注的人界面相似）
     def parse_follower(self, response):
-        pass
+        follower_result = json.loads(response.text)
+        if 'data' in follower_result.keys():
+            for i in follower_result.get('data'):
+                yield scrapy.Request(url=self.user_info_url.format(user_token=i.get('url_token'), include=self.user_info_query), callback=self.parse_user)
+        if 'paging' in follower_result.keys():
+            if follower_result.get('paging').get('is_end') is False:
+                yield scrapy.Request(url=follower_result.get('paging').get('next').replace('https://www.zhihu.com/', 'https://www.zhihu.com/api/v4/'), callback=self.parse_follower)
+
 
     # 测试api接口是否可用的
-    def parse_test(self, response):
-        follow_result = json.loads(response.text)
-        if 'data' in follow_result.keys():
-            print("***")
-            for i in follow_result.get('data'):
-                print(i.get('name'))
-        if 'paging' in follow_result.keys():
-            if follow_result.get('paging').get('is_end') is False:
-                yield scrapy.Request(url=follow_result.get('paging').get('next').replace('https://www.zhihu.com/', 'https://www.zhihu.com/api/v4/'), callback=self.parse_test)
+    # def parse_test(self, response):
+    #     follow_result = json.loads(response.text)
+    #     if 'data' in follow_result.keys():
+    #         print("***")
+    #         for i in follow_result.get('data'):
+    #             print(i.get('name'))
+    #     if 'paging' in follow_result.keys():
+    #         if follow_result.get('paging').get('is_end') is False:
+    #             yield scrapy.Request(url=follow_result.get('paging').get('next').replace('https://www.zhihu.com/', 'https://www.zhihu.com/api/v4/'), callback=self.parse_test)
 
 
 
