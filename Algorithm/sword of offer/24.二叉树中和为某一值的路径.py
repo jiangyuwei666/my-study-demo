b9c428825b4582d376036fcf5d6683787b3103e9
jiangyuwei666 <739843128@qq.com>
2019-04-27 12:17:46 +0800

    algorithm

--- Algorithm/sword of offer/24.二叉树中和为某一值的路径.py
+++ Algorithm/sword of offer/24.二叉树中和为某一值的路径.py
@@ -1,5 +1,8 @@
 """
-
+考虑到要从头到尾去遍历树来查找路径，就只能选择前序遍历
+采用栈的思想，遍历某个节点的时候，进行入栈，再对其左子树进行考察，直至根节点，然后是右子树，是一个递归的过程
+入栈后计算当前站内节点值的总和，如果当前值小于目标值，就这递归遍历
+否则如果当前值等于目标值，先判断是否走到了根节点(否则就不是一条路径)，如果是就将其添加到相应结果集当中
 """
 
 
@@ -39,9 +42,10 @@ class Tree:
         else:
             self.root = node
 
-    def add_path(self, target):
+    def find_path(self, target):
 
         path_stack = []
+        ret = []
 
         def __path(tree):
             if tree:
@@ -50,6 +54,16 @@ class Tree:
                 if current_sum < target:
                     __path(tree.left)
                     __path(tree.right)
+                elif current_sum == target:
+                    if not tree.left and not tree.right:
+                        s = []
+                        for i in path_stack:
+                            s.append(i.val)
+                        ret.append(s)
+                path_stack.pop()
+
+        __path(self.root)
+        return ret
 
     @staticmethod
     def calculate(path_list):
@@ -57,3 +71,9 @@ class Tree:
         for i in path_list:
             result += i.val
         return result
+
+
+tree = Tree(arr_list=[10, 5, 4, 12, 7])
+print(tree.find_path(22))
+tree = Tree(arr_list=[5, 4, 3, 2, 1])
+print(tree.find_path(12))
---
+++ Algorithm/sword of offer/25.复杂链表的复制.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+class Node:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
